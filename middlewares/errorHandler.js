7f550985f6e9401070ad2a010f64570e255decd4
Ek Ae <ekzin15@gmail.com>
2023-02-14 23:02:53 +0700

    error handler

---
+++ middlewares/errorHandler.js
@@ -0,0 +1,28 @@
+// NOT FOUND
+const notFound = (req, res, next) => {
+  const error = new Error(`Not Found: ${req.originalUrl}`);
+  res.status(404);
+  next(error);
+};
+
+// Error Class
+class CustomError extends Error {
+  constructor(statusCode, message) {
+    super(message);
+    this.statusCode = statusCode;
+  }
+}
+
+// Error handler
+const errorHandler = (err, req, res, next) => {
+  console.log(err);
+  const statusCode = err.statusCode;
+  res.status(statusCode);
+  res.json({
+    success: false,
+    message: err.message,
+    stack: err?.stack,
+  });
+};
+
+module.exports = { errorHandler, notFound, CustomError };
