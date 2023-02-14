cc558c7ae4a2f3cc992edb150467579acc40da8c
Ek Ae <ekzin15@gmail.com>
2023-02-14 23:03:07 +0700

    add some func

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 // require module
 const express = require('express');
+const { errorHandler, notFound } = require('./middlewares/errorHandler');
 
 // import routes
 const authRoute = require('./routes/authRoute');
 
 const app = express();
+
+// register middleware
+app.use(express.json());
 
 // register routes
 app.get('/', (req, res) => {
@@ -13,6 +17,10 @@ app.get('/', (req, res) => {
     .send(`<h1 style='text-align:center'>Hello from server side</h1>`);
 });
 
-app.use('api/users', authRoute);
+app.use('/api/users', authRoute);
+
+// error handler
+app.use(notFound);
+app.use(errorHandler);
 
 module.exports = app;
